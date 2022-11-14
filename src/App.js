af0d8d45bb896707743ea03064af9c4016691d09
Jonathan <jonathancarpena99@gmail.com>
2022-11-14 12:45:40 -0800

    Added copy to clipboard, Added some punctation options

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { thousandWords } from './lib/commonWords'
 import { MdSend } from 'react-icons/md'
+import { FiClipboard } from 'react-icons/fi'
 
 
 
@@ -11,6 +12,17 @@ function App() {
   const [output, setOutput] = useState([])
   const [limit, setLimit] = useState(160)
   const sortedThousandWords = thousandWords.sort()
+
+  async function copyToClipboard() {
+    const result = output.join(' ')
+    if ("clipboard" in navigator) {
+      await navigator.clipboard.writeText(result);
+    } else {
+      document.execCommand("copy", true, result);
+    }
+
+    console.log(result)
+  };
 
   function handleInputChange(e) {
     setInput(e.target.value)
@@ -31,7 +43,7 @@ function App() {
 
   function reset() {
     setInput('')
-    setRelated([])
+    setRelated(sortedThousandWords)
   }
 
   function handleCount(num) {
@@ -68,21 +80,27 @@ function App() {
 
 
       <div className='max-w-3xl w-full flex flex-col  space-y-5  '>
+
         <div className='flex space-x-2 items-center bg-white w-max px-3 rounded-lg'>
           <label htmlFor="maxChars" className=''>Max Chars:</label>
           <input id="maxChars" type="number" min={0} onChange={(e) => setLimit(e.target.value)} className="outline-none px-3 py-2" />
         </div>
 
         {/* Output */}
-        <div className='relative min-h-[200px] max-h-[200px] bg-white rounded-xl drop-shadow-xl p-7'>
+        <div className='relative min-h-[200px]  bg-white rounded-xl drop-shadow-xl p-7 overflow-y-auto'>
           {output.length > 0
-            && <p>
+            && <p className='mb-8'>
               {output.map((item, idx) => (
-                <span onClick={() => removeFromOutput(idx)} className='first:capitalize text-xl inline-block cursor-pointer hover:scale-110 active:scale-90 transtion-all ease-in-out duration-150 mr-1.5'>{item}</span>
+                <span key={`${item}-${idx}`} onClick={() => removeFromOutput(idx)} className='first:capitalize text-xl inline-block cursor-pointer hover:scale-110 active:scale-90 transtion-all ease-in-out duration-150 mr-1.5'>{item}</span>
               ))}
             </p>
           }
-          <span className='absolute bottom-5 right-7'>{count}/{limit} chars</span>
+          <div className='flex items-center space-x-3 fixed bottom-5 bg-white right-7'>
+
+            <span className=' '>{count}/{limit} chars</span>
+            <button onClick={copyToClipboard} className='flex items-center bg-[#48c1a1] hover:bg-[#32b592] active:scale-90 rounded-lg px-3 space-x-1 text-white py-1 transition-all ease-in-out duration-150'><FiClipboard /><span>Copy</span></button>
+          </div>
+
         </div>
 
         {/* Suggestions */}
@@ -91,7 +109,7 @@ function App() {
           <ul className='flex flex-wrap items-start  max-h-[350px]  overflow-y-auto w-full '>
             {related.length > 0
               ? related.map((item) => (
-                <li onClick={() => addToOutput(item)} key={item} className="hover:drop-shadow-md hover:scale-105 active:scale-90 cursor-pointer transition-all ease-in-out duration-150 mb-3 mr-3 text-xl bg-white border-[1px] px-2 py-1 rounded-md">
+                <li onClick={() => addToOutput(item)} key={item} className="hover:drop-shadow-md hover:scale-105 active:scale-90 cursor-pointer  transition-all ease-in-out duration-150 mb-3 mr-3 text-xl bg-white border-[1px] px-2 py-1 rounded-md">
                   {item}
                 </li>
               ))
